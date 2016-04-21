d77568bd3bf619fb97ba81e8773ddd0d86acd0ac
HaustWang <wyy123_2008@qq.com>
2016-04-21 14:20:50 +0800

    adding some comment of code

--- basic_http.h
+++ basic_http.h
@@ -17,9 +17,9 @@ public:
 
 public:
     virtual ~BasicHttp(){};
-    
+
     //typedef void (*handler)(std::string query, std::string body);
-    
+
 
     void Init(uint32_t port);
     bool Start();
@@ -33,11 +33,14 @@ public:
 
 protected:
     using handler_map = std::map<std::string, handler>;
+    // this define means that if get reply it will reply the first of the requests in the multimap which has the same uri
+    // it may has a lot of other problems. so this defined can instead by uri+query_string as the map key, like this:
+    // using connnection_map = std::map<std::string, mg_connection*>;
     using connection_map = std::multimap<std::string, mg_connection*>;
 
 private:
     static void EvHandler(struct mg_connection* nc, int ev, void* ev_data);
-    static void HandleRequst(struct mg_connection* nc, int ev, void *ev_data); 
+    static void HandleRequst(struct mg_connection* nc, int ev, void *ev_data);
 
 public:
     static handler_map _handlers;
